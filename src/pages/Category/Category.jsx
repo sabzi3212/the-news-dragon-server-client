62d564dd8e1372388f13f87a3f7ad2593c67472e
sabzi3212 <sabbirahmed.lu1@gmail.com>
2023-04-28 17:51:57 +0600

    news added

---
+++ src/pages/Category/Category.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
+import NewsCard from '../Home/NewsCard/NewsCard';
+
+const Category = () => {
+    const {id} = useParams();
+    const categoryNews = useLoaderData();
+    return (
+        <div>
+            <h2>This is a category: {categoryNews.length}</h2>
+            {
+                categoryNews.map(news => <NewsCard key={news._id} news = {news}></NewsCard>)
+            }
+            
+        </div>
+    );
+};
+
+export default Category;
